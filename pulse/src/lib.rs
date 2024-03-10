d7c8c3fabdfabb93573483a77c84796d5f1dd0c8
wallyflowers <160357178+wallyflowers@users.noreply.github.com>
2024-03-09 21:39:54 -0600

    Update variable name and fix imports

--- pulse/src/lib.rs
+++ pulse/src/lib.rs
@@ -1,4 +1,4 @@
-const source: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/lib.rs"));
+const SOURCE: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/lib.rs"));
 
 mod firstday {
     pub struct Expression<'a> {
@@ -7,19 +7,16 @@ mod firstday {
     }
 
     pub type Quality = f64; // Light and darkness
-
-    pub const Day: f64 = f64::MAX;
-    pub const Night: f64 = f64::MIN;
 }
 
 mod secondday {
-    use crate::source;
-    use crate::firstday::{Expression, Day};
+    use crate::SOURCE;
+    use crate::firstday::Expression;
 
     // The firmament of Heaven
     pub static heaven: Expression = Expression {
-        data: source,
-        quality: Day
+        data: SOURCE,
+        quality: f64::MAX,
     };
 }
 
